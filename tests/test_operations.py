bb85c339eb726c08e82c13af77fb543aada58719
PawanDabi <pawandabi30400400@gmail.com>
2024-12-22 23:55:18 +0530

    changed the test cases

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -2,7 +2,7 @@ from src.math_operations import add,sub
 
 def unit_test_add():
     assert add(5,6) == 11
-    assert add(7,2) == 10, "check your parameters again"
+    assert add(7,2) == 9
     assert add(10,20) == 30
 
 def unit_test_sub():
